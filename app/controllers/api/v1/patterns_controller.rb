d5ac6fbc654ce0c30f3003436684ab4406afb2fd
satub <satu.barnhill@gmail.com>
2016-12-01 11:22:32 -0500

    added skeleton structure for controllers

---
+++ app/controllers/api/v1/patterns_controller.rb
@@ -0,0 +1,36 @@
+module Api
+  module V1
+    class PatternsController < ApplicationController
+      ### GET    /api/v1/patterns(.:format)          api/v1/patterns#index
+      def index
+        render json: Pattern.all.order(:id)
+      end
+
+      ### GET    /api/v1/patterns/:id(.:format)      api/v1/patterns#show
+      def show
+        render json: Pattern.find_by(id: params[:id])
+      end
+
+      def new
+        @pattern = Pattern.new
+      end
+
+      ###  POST   /api/v1/patterns(.:format)          api/v1/patterns#create
+      def create
+      end
+
+      def edit
+      end
+
+      ###  PUT    /api/v1/patterns/:id(.:format)      api/v1/patterns#update
+      def update
+      end
+
+      private
+
+      def pattern_params
+        params.require(:pattern).permit(:name, :width_loops, :height_rows, :repeat_x, :repeat_y)
+      end
+    end
+  end
+end
