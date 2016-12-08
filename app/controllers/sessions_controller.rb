9e0fffe11c77a09fcdfbaa5f3c52bbb271d7bc9b
satub <satu.barnhill@gmail.com>
2016-12-07 20:22:38 -0500

    returns a token when right id info sent in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,13 +1,11 @@
 class SessionsController < ApplicationController
 
-  skip_before_action :authenticate
 
   def create
     user = User.find_by(user_name: auth_params[:user_name])
     if user.authenticate(auth_params[:password])
       jwt = Auth.issue({user: user.id})
       render json: {jwt: jwt}
-    else
     end
   end
 
@@ -15,3 +13,4 @@ class SessionsController < ApplicationController
     def auth_params
       params.require(:auth).permit(:user_name, :password)
     end
+end
